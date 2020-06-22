78439ab0498dfb5e168bb99ca4c91511eaea3664
Narayan <Shashankhegde32@gmaol.com>
2020-06-23 00:40:23 +0530

    First name and second name

--- userReg.sh
+++ userReg.sh
@@ -3,17 +3,25 @@
 echo "Welcome to portal registration"
  
 FirstName(){
- read firstName
+ firstName=$1
  if [[ "${firstName:0:1}" =~ [A-Z] ]]
  then
 	if [[ ${#firstName} < 3 ]]
 	then
 		echo "First name must be at least 3 digits"
+		break
 	else
 		echo "First name done"
 	fi 
  else
 	echo "First letter must be capital"
+	break
  fi
 }
-FirstName
+
+echo "Enter First Name "
+read firstName
+FirstName $firstName
+echo "Enter second Name"
+read secName
+FirstName $secName
