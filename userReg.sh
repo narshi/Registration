23c736783457a0580fdb1ce68eaffd16c60f66dc
Narayan <Shashankhegde32@gmaol.com>
2020-06-23 00:13:01 +0530

    First Name Validation

--- userReg.sh
+++ userReg.sh
@@ -1,3 +1,19 @@
-#! bin/bash/
+#! /bin/bash -x
 
 echo "Welcome to portal registration"
+ 
+FirstName(){
+ read firstName
+ if [[ "${firstName:0:1}" =~ [A-Z] ]]
+ then
+	if [[ ${#firstName} < 3 ]]
+	then
+		echo "First name must be at least 3 digits"
+	else
+		echo "First name done"
+	fi 
+ else
+	echo "First letter must be capital"
+ fi
+}
+FirstName
